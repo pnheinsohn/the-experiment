7d0c5118b92b43eec3edf3856edc76a85e22eced
Paul <pnheinsohn@uc.cl>
2020-09-10 03:21:03 -0300

    feat/Server Client Rooms

---
+++ Assets/Scripts/LobbyMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] public GameObject landingPagePanel = null;
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/NetworkManagerLobby.cs
@@ -0,0 +1,142 @@
+﻿using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    [SerializeField] private int minPlayers;
+    [Scene] [SerializeField] private string lobbyScene = string.Empty;
+
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+    [Header("Game")]
+    [SerializeField] private NetworkGamePlayerLobby gamePlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+    public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby>();
+    public List<NetworkGamePlayerLobby> GamePlayers { get; } = new List<NetworkGamePlayerLobby>();
+
+    public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+
+    public override void OnStartClient()
+    {
+        var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+
+        foreach (var prefab in spawnablePrefabs)
+        {
+            ClientScene.RegisterPrefab(prefab);
+        }
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+
+        OnClientDisconnected?.Invoke();
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().path != lobbyScene)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        if (SceneManager.GetActiveScene().path == lobbyScene)
+        {
+            NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            roomPlayerInstance.IsLeader = RoomPlayers.Count == 0;
+
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+
+    public override void OnStopServer()
+    {
+        RoomPlayers.Clear();
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (conn.identity != null)
+        {
+            var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+            RoomPlayers.Remove(player);
+            NotifyPlayersOfReadyState();
+        }
+
+        base.OnServerDisconnect(conn);
+    }
+
+    public void NotifyPlayersOfReadyState()
+    {
+        foreach (var player in RoomPlayers)
+        {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+
+    private bool IsReadyToStart()
+    {
+        if (numPlayers < minPlayers) { return false; }
+
+        foreach (var player in RoomPlayers)
+        {
+            if (!player.IsReady) { return false; }
+        }
+
+        return true;
+    }
+
+    public void StartGame()
+    {
+        if (SceneManager.GetActiveScene().path == lobbyScene)
+        {
+            if (!IsReadyToStart()) { return; }
+
+            ServerChangeScene("Scene 1");
+        }
+    }
+
+    public override void ServerChangeScene(string newSceneName)
+    {
+        if (SceneManager.GetActiveScene().path == lobbyScene && newSceneName.StartsWith("Scene"))
+        {
+            for (int i = RoomPlayers.Count - 1; i >= 0; i--)
+            {
+                var conn = RoomPlayers[i].connectionToClient;
+                var gamePlayerInstance = Instantiate(gamePlayerPrefab);
+                gamePlayerInstance.SetDisplayName(RoomPlayers[i].DisplayName);
+
+                NetworkServer.Destroy(conn.identity.gameObject);
+
+                NetworkServer.ReplacePlayerForConnection(conn, gamePlayerInstance.gameObject);
+            }
+        }
+
+        base.ServerChangeScene(newSceneName);
+    }
+}
