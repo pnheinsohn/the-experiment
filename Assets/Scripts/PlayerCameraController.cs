29b92270dea50cf7f6a247bde1469a7d2a7e1704
Paul <pnheinsohn@uc.cl>
2020-09-24 18:22:49 -0300

    ✨ feat: local camera for players

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerCameraController : NetworkBehaviour
+{
+    [Header("Camera")]
+    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
+
+    private CinemachineTransposer transposer;
+
+    public override void OnStartAuthority()
+    {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        virtualCamera.gameObject.SetActive(true);
+        enabled = true;
+
+        virtualCamera.m_Lens.OrthographicSize = 1.2f;
+    }
+}
